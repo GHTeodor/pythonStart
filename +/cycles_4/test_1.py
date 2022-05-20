173fbb6397ff2fa52404de33712126934b44ca39
GHTeodor <Theodor.Lviv@gmail.com>
2022-05-20 18:27:59 +0300

    cycle

---
+++ +/cycles_4/test_1.py
@@ -0,0 +1,14 @@
+"""
+Вивести у консоль перші 100 чисел Фібоначчі (див. у рекомендованих ресурсах)
+wikiwand.com/uk/Послідовність_Фібоначчі
+start = 0
+"""
+
+start, prev = 0, 1
+for i in range(100):
+    print(start)
+    fibN = start + prev
+    print(f"{str(start)}|{str(prev)}|{str(fibN)}")
+    start = prev
+    prev = fibN
+    print(f"{str(start)}|{str(prev)}|{str(fibN)}")
